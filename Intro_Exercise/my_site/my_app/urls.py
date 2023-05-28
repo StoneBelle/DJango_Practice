f28ee5d9e004410d8abc1c85b9d81872a9f5a244
Stone <65476500+StoneBelle@users.noreply.github.com>
2023-05-28 09:26:31 -0400

    Delete Intro_Exercise/my_site directory

--- Intro_Exercise/my_site/my_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index") # shwos up at /my_apps --PROJECT LVL urls.py
-]
--- Intro_Exercise/my_site/my_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-# You can have function or class views
-
-def index(request):
-    return HttpResponse("Hello this is a view inside my_app")
